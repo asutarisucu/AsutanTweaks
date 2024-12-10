cebd365ad3bc7573beea7544403f10fda749b79d
asutarisucu <asutaricusu.s@gmail.com>
2024-12-10 18:02:46 +0900

    Disable void Dive Fixed

--- src/main/java/org/asutarisucu/tweak/DisableVoidDive/DisableVoidDive.java
+++ src/main/java/org/asutarisucu/tweak/DisableVoidDive/DisableVoidDive.java
@@ -1,8 +1,10 @@
 package org.asutarisucu.tweak.DisableVoidDive;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
@@ -17,6 +19,7 @@ import org.asutarisucu.Utiles.Inventory.Inventorys;
 import org.asutarisucu.Utiles.PlayerEntity.InputBlocker;
 
 public class DisableVoidDive {
+    private static int count=0;
     public static void safeVoidDive(String dim){
         MinecraftClient client=MinecraftClient.getInstance();
         PlayerEntity player=client.player;
@@ -34,15 +37,38 @@ public class DisableVoidDive {
                 int RocketSlot= Inventorys.findItemSlot(client,Items.FIREWORK_ROCKET);
                 //花火を持っていないかエリトラをつけていないなら弾く
                 if(RocketSlot!=-1&&player.getEquippedStack(EquipmentSlot.CHEST).getItem()==Items.ELYTRA){
-                    if(!player.isFallFlying()){
-                    }
                     Screen screen=client.currentScreen;
-                    ScreenHandler handler=player.currentScreenHandler;
-                    int mainSlot=player.getInventory().selectedSlot;
-                    if(screen==null&&handler instanceof PlayerScreenHandler&&player.getVelocity().y<0){
-                        client.interactionManager.clickSlot(handler.syncId,RocketSlot,mainSlot, SlotActionType.SWAP,player );
-                        player.setPitch(-90);
-                        client.interactionManager.interactItem(player, Hand.MAIN_HAND);
+                    //エリトラが開いていないとき
+                    if(!player.isFallFlying()){
+                        //ジャンプキーをすでに押している場合
+                        if(client.options.jumpKey.isPressed()){
+                            //GUIを開いていないとき
+                            if(screen==null){
+                                client.execute(()->{
+                                    client.setScreen(new InventoryScreen(player));
+                                });
+                                count=5;
+                                //GUIを開いているとき
+                            }else {
+                                if(count<=0){
+                                    client.execute(()->{
+                                        client.setScreen(null);
+                                    });
+                                }else count--;
+                            }
+                        //ジャンプキーを押していない場合
+                        }else {
+                            client.options.jumpKey.setPressed(true);
+                        }
+                    //エリトラを開いているなら上を向いて花火を使用
+                    }else{
+                        ScreenHandler handler=player.currentScreenHandler;
+                        int mainSlot=player.getInventory().selectedSlot;
+                        if(handler instanceof PlayerScreenHandler&&player.getVelocity().y<0){
+                            client.interactionManager.clickSlot(handler.syncId,RocketSlot,mainSlot, SlotActionType.SWAP,player );
+                            player.setPitch(-90);
+                            client.interactionManager.interactItem(player, Hand.MAIN_HAND);
+                        }
                     }
                 }else if(Configs.Generic.VOID_DISCONNECT.getBooleanValue())disconnectFromServer(client);
             }
